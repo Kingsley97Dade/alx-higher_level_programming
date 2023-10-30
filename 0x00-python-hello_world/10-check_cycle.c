6bff9dbdb4c1c1a6c0c3a63e62d42ed72de041f3
Kingsley97Dade <kingsleydade@gmail.com>
2023-10-30 15:39:32 -0700

    Header file

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 
 /**
- * check_cyc - Checks if a linked list has a cycle
+ * check_cycle - Checks if a linked list has a cycle
  * @list: linked list to check
  *
  * Return: 1 if the list has a cycle, 0 if it doesn't
  */
-int check_cyc(listint_t *list)
+int check_cycle(listint_t *list)
 {
 	listint_t *slow = list;
 	listint_t *fast = list;
