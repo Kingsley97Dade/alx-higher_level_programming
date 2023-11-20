f406115f7fe598957b13296d2b3f24221b794546
Kingsley97Dade <kingsleydade@gmail.com>
2023-11-20 13:27:58 -0800

    Corrected Tasks 7 and 8

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -3,7 +3,6 @@ from __future__ import print_function
 import sys
 
 def safe_function(fct, *args):
-    """executes a function safely"""
     try:
         res = fct(*args)
     except Exception as e:
