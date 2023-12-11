933532180536e71b1feda2f153187a1674e2a43a
Kingsley97Dade <kingsleydade@gmail.com>
2023-12-11 11:38:34 -0800

    Python Almost a circle

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+class Base:
+    '''Representing a base'''
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            type(self).__nb_objects += 1
+            self.id = type(self).__nb_objects
