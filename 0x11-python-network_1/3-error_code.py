8b6ba7c0b4fa375d4665891d59571c38bcfba573
Kingsley97Dade <kingsleydade@gmail.com>
2024-03-01 16:45:57 -0800

    Project on Python networking part 1

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Takes in a URL, sends a request to the URL and displays the body of the response (decoded in utf-8)."""
+from urllib.request import Request, urlopen
+from urllib.error import HTTPError
+from sys import argv
+
+if __name__ == '__main__':
+    req = Request(argv[1])
+    try:
+        with urlopen(req) as response:
+            r = response.read()
+            print(r.decode('utf-8'))
+    except HTTPError as e:
+        print('Error code:', e.code)
