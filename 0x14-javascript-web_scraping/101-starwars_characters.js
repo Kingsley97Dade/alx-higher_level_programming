78be93e7d24310dc1215967e21a5609b3210ff38
Kingsley97Dade <kingsleydade@gmail.com>
2024-03-26 06:59:59 -0700

    Corrected Task 8

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,45 +1,32 @@
 #!/usr/bin/node
 
-'use strict';
-
-const request = require('request');
-
-// Check if the movie ID argument is provided
-if (process.argv.length < 3) {
-  console.log('Usage: ./101-starwars_characters.js <movie_id>');
-  process.exit(1);
-}
+// shows all the characters of a Star Wars movie in the right order
+const req = require('request');
 
 const movieId = process.argv[2];
 const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
-// Make a GET request to the Star Wars API endpoint to fetch movie information
-request(apiUrl, (err, response, body) => {
-  if (err) {
-    console.error('An error occurred while making the GET request:', err);
+req(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
   } else {
-    if (response.statusCode !== 200) {
-      console.error('Failed to fetch movie information:', response.statusMessage);
-    } else {
-      const movieInfo = JSON.parse(body);
-      const charactersUrls = movieInfo.characters;
-
-      // Fetch and print each character's name in the order they appear in the movieInfo.characters array
-      charactersUrls.forEach((characterUrl, index) => {
-        request.get(characterUrl, (charErr, charResponse, charBody) => {
-          if (charErr) {
-            console.error('An error occurred while fetching character data:', charErr);
-          } else {
-            if (charResponse.statusCode !== 200) {
-              console.error('Failed to fetch character information:', charResponse.statusMessage);
-            } else {
-              const characterInfo = JSON.parse(charBody);
-              console.log(`${index + 1}. ${characterInfo.name}`);
-            }
-          }
-        });
-      });
-    }
+    const charactersUrls = JSON.parse(body).characters;
+    printCharactersNames(charactersUrls, 0);
   }
 });
 
+function printCharactersNames (charactersUrls, index) {
+  if (index >= charactersUrls.length) {
+    return;
+  }
+
+  req(charactersUrls[index], (error, response, body) => {
+    if (error) {
+      console.error(error);
+    } else {
+      const characterName = JSON.parse(body).name;
+      console.log(characterName);
+      printCharactersNames(charactersUrls, index + 1);
+    }
+  });
+}
